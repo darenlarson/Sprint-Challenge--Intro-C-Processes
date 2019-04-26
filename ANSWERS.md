bb77fea31ff617655efc4db8e28cac9b8b849e36
darenlarson <larson.daren@gmail.com>
2019-04-26 12:44:38 -0500

    Forgot to commit while panic googling the last 3 hours.

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,14 +1,20 @@
 **1. List all of the main states a process may be in at any point in time on a standard Unix system. Briefly explain what each of these states means.**
+Created - a process has been created, but has not been run yet.
+Sleep - a process has been loaded and started running, but is waiting to continue running.
+Zombie - a process is done and waiting to be terminated.
+Running - a process is currenting being executed by the OS
 
 
 
 **2. What is a zombie process?**
-
+A process whoses execution is completed, but it remains in the process table, waiting for an exit() system call.
 
 
 **3. How does a zombie process get created? How does one get destroyed?**
-
+If, for example, the parent of a child zombie process fails to eventually call exit(), the zombie process may persist and cause issues.
+Per google, you can also kill zombies using the command kill -s SIGCHILD
 
 
 **4. What are some of the benefits of working in a compiled language versus a non-compiled language? More specifically, what benefits are there to be had from taking the extra time to compile our code?**
-
+Compiled languages tend to be faster than non-compiled langauges because there is no intermediate interpretor layer (you're writing in the native code of the machine).
+You can write more optimized code for the machine you're running it on.
--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,78 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-/**
- * Main
- */
+
 int main(int argc, char **argv)
 {
-  // Parse command line
 
-  // Open directory
+  // -------- Parse command line --------
+  char *dir_path;
 
-  // Repeatly read and print entries
+  if (argc > 1) {
+    dir_path = argv[1];
+
+    // if no additional commandline args entered, use the current directory
+  } else {
+    dir_path = ".";
+  }
+
+
+  // -------- Open directory: call opendir() --------
+  // Pass the dir_path entered into the command line into DIR directory. This opens the directory stored in char `dir_path` and returns a POINTER to a variable of type DIR.
+  DIR *d = opendir(dir_path);
+
+  // opendir() returns NULL if there is an error
+  if (d == NULL) {
+    printf("%s is an invalid directory\n", dir_path);
+    exit(1);
+  }
+
+
+  // -------- Repeatly read and print entries: call readdir() --------
+  struct dirent *ent;
+  struct stat buf;
+
+  printf("\nType\t Size\t Name\n");
+  
+  while ((ent = readdir(d)) != NULL) {
+    stat(ent->d_name, &buf);
+
+    // --- printf() for MVP ---
+    // printf("%lld\t %s\n", buf.st_size, ent->d_name);
+
+
+    // --- STRETCH ---
+    if (buf.st_mode == 16877) {
+      char *type = "<DIR>";
+
+      printf("%s\t %lld\t %s\n", type, buf.st_size, ent->d_name);
+
+      DIR *sub_d = opendir(ent->d_name);
+        while((ent = readdir(sub_d)) != NULL) {
+          stat(ent->d_name, &buf);
+          if (buf.st_mode == 16877) {
+            type = "<DIR>";
+            printf("\t %s\t %lld\t %s\n", type, buf.st_size, ent->d_name);
+          } else {
+            printf("\t %hu\t %lld\t %s\n", buf.st_mode, buf.st_size, ent->d_name);
+          }
+        }
+
+    } else {
+      printf("%hu\t %lld\t %s\n", buf.st_mode, buf.st_size, ent->d_name);
+    }
+
+
+  }
+
+  printf("\n");
 
   // Close directory
+  closedir(d);
+
 
   return 0;
 }
